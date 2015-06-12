937cb15ba54ecc3c75fca4cb6445672851ce1473
Rubysoft <uriklar@gmail.com>
2015-06-12 21:08:51 +0300

    include business in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,29 @@ class User < ActiveRecord::Base
   before_save -> { skip_confirmation! }
   before_validation :get_invitation, on: :create
 
-  has_one :businesses
+  has_one :business
   has_many :sent_invitations, :class_name => 'Invitation', :foreign_key => 'sender_id'
 	belongs_to :invitation
 
   attr_accessor :invitation_code
 
-  private
-  def get_invitation
-    self.invitation = Invitation.find_by_token(self.invitation_code)
+  default_scope { includes(:business) }
+
+  def serializable_hash(options={})
+    options = { 
+      :include => {
+        :business => { 
+          :include => [:links, :businesses_in_network] 
+        }
+      }
+    }.update(options)
+    super(options)
   end
+
+  private
+
+
+    def get_invitation
+      self.invitation = Invitation.find_by_token(self.invitation_code)
+    end
 end
